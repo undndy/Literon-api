25774f42c1cba5c0517d0e00a1c59daea7ad3c41
undndy <alisher.toiguliev1@mail.ru>
2023-08-25 17:28:19 +0600

    upload project

--- src/main/java/com/lib/litron10release/controller/BookController.java
+++ src/main/java/com/lib/litron10release/controller/BookController.java
@@ -1,12 +1,17 @@
 package com.lib.litron10release.controller;
 
 import com.lib.litron10release.DAO.impl.BookService;
+import com.lib.litron10release.entity.Book;
+import com.lib.litron10release.entity.FileItem;
 import com.lib.litron10release.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @RestController
@@ -14,6 +19,16 @@ import java.io.IOException;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/{id}")
+    private ResponseEntity<?> getImageById(@PathVariable("id") Long id){
+        Book book = bookRepository.findById(id).orElse(null);
+        return ResponseEntity.ok().header("bookName", book.getName())
+                .contentType(MediaType.valueOf(book.getContentType()))
+                .body(new InputStreamResource(new ByteArrayInputStream(book.getImageBytes())));
+    }
 
     @PostMapping("/book")
     private ResponseEntity<?> uploadState(@RequestParam("book") MultipartFile book) throws IOException {
